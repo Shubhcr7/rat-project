27813b5a8985c69dd44fb5a9e301c4eec62258e9
Shubhcr7 <shubhamsharmacr7@gmail.com>
2018-05-30 19:24:39 +0530

    batch-register half done

--- src/app/batch-register/batch-register.component.ts
+++ src/app/batch-register/batch-register.component.ts
@@ -1,5 +1,7 @@
+import { element } from 'protractor';
+import { Http } from '@angular/http';
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-batch-register',
   templateUrl: './batch-register.component.html',
@@ -8,43 +10,44 @@ import { Component, OnInit } from '@angular/core';
 export class BatchRegisterComponent implements OnInit {
 
  //subjects model comes here
- subjects=[
-  {
-    name:'Core_Java'
-  },
-  {
-    name:'Oracle_SQL'
-  },
-  {
-    name:'Php'
-  },
-  {
-    name:'AdminI'
-  },
-  {
-    name:'Android'
-  },
-  {
-    name:'Python'
-  },
-];
-//eligible students
-eligible=[
-{
-
-},
-{
-
-},
-{
-
-},
-{
-
-},
-];
-constructor() { }
-
+ subjects;
+students;
+validstudents;
+constructor(http:Http) {
+  http.get('http://localhost:3000/course/allcourse/').subscribe(res=>{
+    this.subjects=res.json();
+  })
+ }
+getValidStudents(x){
+  // this.http('http://localhost:3000/course')
+  this.students=[
+    {
+    name:'Saransh Jain',
+    individual_courses:['Bigdata_Hadoop','AdminII','Php']
+    },
+    {
+      name:'Shubham Sharma',
+      individual_courses:['Core_Java','Php','AdminII','Android']
+    },
+    {
+      name:'Kunal Mittal',
+      individual_courses:['AdminII','Core_Java','Advance_Java']
+    }
+  ];
+  this.validstudents=null;
+  this.students.forEach(element => {
+  let y=$("select option:selected").text();
+    for(var i in element.individual_courses)
+    {
+      var x=element.individual_courses[i];
+      if(y===x)
+      {
+        
+      }
+    }
+    }
+  );
+}
 ngOnInit() {
 }
 
